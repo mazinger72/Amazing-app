5ad5d7f083e0ca2e7b246f782d7a5539cbe2d228
Rick Barros <rickbarros@icloud.com>
2021-01-09 00:37:35 -0500

    Commit to github

---
+++ backend/utils.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+import jwt from 'jsonwebtoken';
+
+export const generateToken = (user) => {
+  return jwt.sign(
+    {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+    process.env.JWT_SECRET || 'somethingsecret',
+    {
+      expiresIn: '30d',
+    }
+  );
+};
