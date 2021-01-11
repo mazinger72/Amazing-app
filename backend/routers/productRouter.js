54035b8fcf17dc7fc9beba3765f3b535c7667df5
Rick Barros <rickbarros@icloud.com>
2021-01-11 10:35:14 -0500

    removed static id numbers

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -5,26 +5,20 @@ import Product from '../models/productModel.js';
 
 const productRouter = express.Router();
 
-productRouter.get(
-    '/',
-    expressAsyncHandler(async (req, res) => {
+productRouter.get('/', expressAsyncHandler(async (req, res) => {
       const products = await Product.find({});
       res.send(products);
     })
   );
 
-productRouter.get(
-    '/seed',
-    expressAsyncHandler(async (req, res) => {
+productRouter.get('/seed', expressAsyncHandler(async (req, res) => {
         // await Product.remove({});
         const createdProducts = await Product.insertMany(data.products);
         res.send({ createdProducts });
     })
 );
 
-productRouter.get(
-    '/:id',
-    expressAsyncHandler(async (req, res) => {
+productRouter.get('/:id', expressAsyncHandler(async (req, res) => {
       const product = await Product.findById(req.params.id);
       if (product) {
         res.send(product);
